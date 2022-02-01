5fea4f1a86cc4f7c7717a6eb97987e45fcf196e5
Marc Sànchez <msanxes@institutmontilivi.cat>
2022-02-01 21:03:12 +0100

    feat: connection created

---
+++ dbConnection.js
@@ -0,0 +1,25 @@
+const mongoose = require("mongoose");
+
+require("dotenv").config();
+
+let mongoConnection = null;
+
+const connectDB = async () => {
+  const username = process.env.MONGODB_USER;
+  const password = process.env.MONGODB_PASSWORD;
+  const host = process.env.MONGODB_HOST;
+  const port = process.env.MONGODB_PORT;
+  const database = process.env.MONGODB_DATABASE;
+
+  const connectionString = `mongodb://${username}:${password}@${host}:${port}/${database}`;
+  return await mongoose.connect(connectionString);
+};
+
+const getConnection = async () => {
+  if (!mongoConnection) {
+    mongoConnection = await connectDB();
+  }
+  return mongoConnection;
+};
+
+module.exports = { getConnection };
