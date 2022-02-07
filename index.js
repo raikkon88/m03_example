f7668b0afce79949066233f696eeb75df89f6c92
Marc Sànchez <msanxes@institutmontilivi.cat>
2022-02-07 18:34:43 +0100

    feat: added nodemon and user model

--- index.js
+++ index.js
@@ -3,9 +3,14 @@ const { getConnection } = require("./dbConnection");
 const app = express();
 const port = 3000;
 
+const User = require("./model/user.model");
+
 app.get("/", async (req, res) => {
   const connection = await getConnection();
-  res.send("Hello World!");
+  const dbRes = await User.find({});
+
+  console.log(dbRes);
+  return res.send("Hello World!");
 });
 
 app.listen(port, () => {
