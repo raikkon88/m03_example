c72ec065d5a590bcab715491f7e7ddb53607ba90
Marc Sànchez <msanxes@institutmontilivi.cat>
2022-02-07 21:07:05 +0100

    feat: Added log layer, post user created

--- index.js
+++ index.js
@@ -1,20 +1,34 @@
 const express = require("express");
+const logger = require("./log/logger.log");
 const { getConnection, disconnectDB } = require("./dbConnection");
 const app = express();
 const port = 3000;
+
+app.use(express.json());
 
 const User = require("./model/user.model");
 
 app.get("/", async (req, res) => {
   const dbRes = await User.find({});
 
-  console.log(dbRes);
   return res.send("Hello World!");
 });
 
+app.post("/user", async (req, res) => {
+  try {
+    const user = await User.create({
+      username: req.body.username,
+      password: req.body.password,
+    });
+    return res.send(user);
+  } catch (err) {
+    logger.error("Ei que craixejo, " + JSON.stringify(err.message));
+    return res.status(500).send("unable to create user");
+  }
+});
+
 const server = app.listen(port, async () => {
-  const db = await getConnection();
-  console.log(db);
+  await getConnection();
   console.log(`Example app listening on port ${port}`);
 });
 
